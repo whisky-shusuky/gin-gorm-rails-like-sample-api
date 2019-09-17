cbfe78281f55944dc11d02b203ab2fc5208eaccb
whisky shusuky <whisky.shusuky@gmail.com>
2019-09-18 00:59:30 +0900

    feat(mysql): setup mysql.

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-gorm-tutorial/db"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Hello, World")
 	})
+	db.Init()
 	r.Run()
+	db.Close()
 }
