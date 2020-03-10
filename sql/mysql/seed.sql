67ced67ea811cbdccc7c6d81c0c9feb19df547be
whisky shusuky <whisky.shusuky@gmail.com>
2020-03-10 21:04:39 +0900

    feat(sql): add seed.

---
+++ sql/mysql/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO `shops` ( shop_name, shop_description) VALUES
+('whisky shop','Shop of liquors.'),
+('shusuky no mise', 'Sake suki oyaji no mise.');
+
+INSERT INTO `books` ( book_name, book_description, sales) VALUES
+('whisky diary','Secret diary of whisky!', null),
+('shusuky diary', 'Secret diary of whisky!', 20),
+('book of whisky','A book about world whisky!', 200),
+('book of shusuky','A book about world nonbei people!', 20000);
